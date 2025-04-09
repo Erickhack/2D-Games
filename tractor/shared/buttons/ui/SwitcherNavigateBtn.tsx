2e223d6538f3487e7516b079143d496662753d07
ErickHack <blackdragoneraj@gmail.com>
2025-04-09 20:53:57 +0500

    feat: returned back feature in switcher navigate btn

--- tractor/shared/buttons/ui/SwitcherNavigateBtn.tsx
+++ tractor/shared/buttons/ui/SwitcherNavigateBtn.tsx
@@ -26,35 +26,33 @@ export const SwitcherNavigateBtn = () => {
   }, [url]);
 
   return (
-    <If conditional={url?.pathname !== '/'}>
-      <div className="relative flex rounded-[20px] bg-[#E8EEF9] p-2">
-        <If conditional={Boolean(active)}>
-          <div
-            className="absolute top-2 left-2 h-[42px] w-[151px] rounded-xl bg-[#047EFD] transition-transform duration-300"
-            style={{
-              transform:
-                active === 'history' ? 'translateX(0)' : 'translateX(151px)',
-            }}
-          />
-        </If>
+    <div className="relative flex rounded-[20px] bg-[#E8EEF9] p-2">
+      <If conditional={Boolean(active)}>
+        <div
+          className="absolute top-2 left-2 h-[42px] w-[151px] rounded-xl bg-[#047EFD] transition-transform duration-300"
+          style={{
+            transform:
+              active === 'history' ? 'translateX(0)' : 'translateX(151px)',
+          }}
+        />
+      </If>
 
-        <button
-          className={`z-10 h-[42px] w-[151px] px-[36px] py-2.5 transition-colors duration-300 ${
-            active === 'history' ? 'text-white' : 'text-[#047EFD]'
-          }`}
-          onClick={handleHistoryBtn}
-        >
-          История
-        </button>
-        <button
-          className={`z-10 h-[42px] w-[151px] px-[36px] py-2.5 transition-colors duration-300 ${
-            active === 'simulation' ? 'text-white' : 'text-[#047EFD]'
-          }`}
-          onClick={handleSimulationBtn}
-        >
-          Симуляция
-        </button>
-      </div>
-    </If>
+      <button
+        className={`z-10 h-[42px] w-[151px] px-[36px] py-2.5 transition-colors duration-300 ${
+          active === 'history' ? 'text-white' : 'text-[#047EFD]'
+        }`}
+        onClick={handleHistoryBtn}
+      >
+        История
+      </button>
+      <button
+        className={`z-10 h-[42px] w-[151px] px-[36px] py-2.5 transition-colors duration-300 ${
+          active === 'simulation' ? 'text-white' : 'text-[#047EFD]'
+        }`}
+        onClick={handleSimulationBtn}
+      >
+        Симуляция
+      </button>
+    </div>
   );
 };
