19f3766f0c27c8cf5c794b16d6de52902349a8a3
ErickHack <blackdragoneraj@gmail.com>
2025-04-21 16:42:26 +0500

    feat: added raio puzl

--- app/routes/radio/simulation.tsx
+++ app/routes/radio/simulation.tsx
@@ -1,5 +1,29 @@
 import SimulationPage from 'pages/simulators/ui';
 
+const CORRECT_POSITIONS = [
+  { x: 586, y: 325 },
+  { x: 665, y: 330 },
+  { x: 530, y: 230 },
+  { x: 530, y: 475 },
+  { x: 762, y: 320 },
+  { x: 770, y: 436 },
+];
+
+const PIECE_SIZES = [
+  { width: 374, height: 136 },
+  { width: 517, height: 469 },
+  { width: 134, height: 152 },
+  { width: 134, height: 154 },
+  { width: 152, height: 43 },
+  { width: 166, height: 192 },
+];
+
 export default function Simulation() {
-  return <SimulationPage />;
+  return (
+    <SimulationPage
+      CORRECT_POSITIONS={CORRECT_POSITIONS}
+      PIECE_SIZES={PIECE_SIZES}
+      puzlSourcePath="radio"
+    />
+  );
 }
